68c88648d99165545a59a409a2e8876743b5146f
prakashrajcdr <prakashrajcdr@gmail.com>
2023-08-24 23:23:32 +0530

    with feign client and eureka client load balancer

--- Micro-Student/src/main/java/com/prakash/ProjectJpa/service/StudentService.java
+++ Micro-Student/src/main/java/com/prakash/ProjectJpa/service/StudentService.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.prakash.ProjectJpa.FeignClient.AddressDetailAPI;
 import com.prakash.ProjectJpa.entity.StudentDetail;
 import com.prakash.ProjectJpa.repository.StudentRepository;
 
@@ -19,8 +20,8 @@ public class StudentService {
 	@Autowired
 	private StudentRepository studentRepository;
 	
-//	@Autowired
-//	private AddressDetailAPI addressDetailAPI;
+	@Autowired
+	private AddressDetailAPI addressDetailAPI;
 	
 	@Autowired
 	LoadBalancerClient loadBalancerClient;
@@ -39,17 +40,24 @@ public class StudentService {
 		
 //		Map<String, Object> addRes = addressDetailAPI.getAddressDetailById(id);
 		
+		// we can get the service instance manually -  loadbalancer provide service Instance by round robin 
 		ServiceInstance serviceInstance = loadBalancerClient.choose("address-service"); // should be an address-service application name
 		String uri = serviceInstance.getUri().toString();
 		String contextPath = serviceInstance.getMetadata().get("contextPath");
 		System.out.println("serviceInstance uri + contextPath - " + uri + contextPath);
 
-		Map<String, Object> addRes = webClient
-										.get()
-										.uri(uri + contextPath + "/getAddress/" + id)
-										.retrieve()
-										.bodyToMono(HashMap.class)
-										.block();
+		Map<String, Object> addRes = null;
+		
+		// using web client
+//		addRes = webClient
+//					.get()
+//					.uri(uri + contextPath + "/getAddress/" + id)
+//					.retrieve()
+//					.bodyToMono(HashMap.class)
+//					.block();
+		
+		// using feign client - eureka has loadbalancer inside
+		addRes = addressDetailAPI.getAddressDetailById(id);
 		
 		res.put("studentDetail", studentDetail);
 		res.put("addressDetail", addRes);
